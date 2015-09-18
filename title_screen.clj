da49a90e102f85c416c558b22a08ef973740d715
Sal <physicsfu@gmail.com>
2015-09-17 20:19:00 -0400

    THANK YOU JESUS FOR PROTOCOLS

--- title_screen.clj
+++ title_screen.clj
@@ -4,13 +4,17 @@
         perished.character)
   (:import [UnityEngine
             Application
-            PlayerPrefs]))
+            PlayerPrefs
+            Debug]))
 
-(defn- new-game [] 
-  (PlayerPrefs/SetString "Scene" "new-game")
-  (PlayerPrefs/SetString "Party" (str (random-party)))
-  (Application/LoadLevel (get-scene :new-game)))
-(defn- exit [] (Application/Quit))
+(defprotocol StartMenuEvents
+  (NewGame [this])
+  (Exit [this]))
 
-(defcomponent TriggerNewGame [] (OnEnable [this] (new-game)))
-(defcomponent TriggerExit [] (OnEnable [this] (exit)))
+(defcomponent StartMenu []
+  StartMenuEvents
+  (NewGame [this] 
+    (PlayerPrefs/SetString "Scene" "new-game")
+    (PlayerPrefs/SetString "Party" (str (random-party)))
+    (Application/LoadLevel (get-scene :new-game)))
+  (Exit [this] (Application/Quit)))
